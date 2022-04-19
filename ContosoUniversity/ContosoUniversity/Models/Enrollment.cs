b30897a8d982630c55fa5660337de9c55a4dd091
LeeTrent <lee.trent.1@gmail.com>
2022-04-19 12:42:12 -0500

    Completed creation of complex data model

--- ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -10,12 +10,24 @@ namespace ContosoUniversity.Models
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
-        public int CourseID { get; set; }
-        public int StudentID { get; set; }
+
+
         [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
+        /////////////////////////////////////////////////////////////////////////
+        // An enrollment record is for one course,
+        // so there's a CourseID FK property and a Course navigation property
+        /////////////////////////////////////////////////////////////////////////
+        public int CourseID { get; set; }
         public Course Course { get; set; }
+
+
+        /////////////////////////////////////////////////////////////////////////
+        // An enrollment record is for one student,
+        // so there's a StudentID FK property and a Student navigation property
+        /////////////////////////////////////////////////////////////////////////
+        public int StudentID { get; set; }
         public Student Student { get; set; }
     }
 }
