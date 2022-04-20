fd6f32e2314b216c36ab3e722664f8a847bb9a8f
LeeTrent <lee.trent.1@gmail.com>
2022-04-20 13:18:47 -0500

    Scaffolded instructors

---
+++ ContosoUniversity/ContosoUniversity/Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using ContosoUniversity.Models;
+using ContosoUniversity.Models.SchoolViewModels;  // Add VM
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public InstructorIndexData InstructorData { get; set; }
+        public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? courseID)
+        {
+            InstructorData = new InstructorIndexData();
+            InstructorData.Instructors = await _context.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses)
+                    .ThenInclude(c => c.Department)
+                .OrderBy(i => i.LastName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                InstructorID = id.Value;
+
+                //////////////////////////////////////////////////////////////////////////////////////////////////////
+                // The Single method is used on a collection when the collection has only one item.
+                // The Single method throws an exception if the collection is empty or if there's more than one item.
+                // An alternative is SingleOrDefault, which returns a default value if the collection is empty.
+                // For this query, null in the default returned.
+                //////////////////////////////////////////////////////////////////////////////////////////////////////
+                Instructor instructor = InstructorData.Instructors.Where(i => i.ID == id.Value).Single();
+
+                InstructorData.Courses = instructor.Courses;
+            }
+
+            if (courseID != null)
+            {
+                CourseID = courseID.Value;
+                var selectedCourse = InstructorData.Courses
+                    .Where(x => x.CourseID == courseID).Single();
+                await _context.Entry(selectedCourse)
+                              .Collection(x => x.Enrollments).LoadAsync();
+                foreach (Enrollment enrollment in selectedCourse.Enrollments)
+                {
+                    await _context.Entry(enrollment).Reference(x => x.Student).LoadAsync();
+                }
+                InstructorData.Enrollments = selectedCourse.Enrollments;
+            }
+        }
+    }
+}
