e5be160c27159182c6fe6db7c20b5114364c2b95
leetrent <10787590+leetrent@users.noreply.github.com>
2022-04-23 08:57:11 -0500

    Part5 complex data model (#2)
    * Redefined Student table and added annotations to Student model class
    
    * Changed create student to use Student model instead of view model
    
    * Completed creation of complex data model
    
    * Scaffolded courses
    
    * Scaffolded instructors
    
    * Course create page now renders a Department dropdown
    
    * Course edit  page now renders a Department dropdown
    
    * Completed Course CRUD operations
    
    * Completed CRUD operation for Instructor pages

---
+++ ContosoUniversity/ContosoUniversity/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class CreateModel : DepartmentNamePageModel // Derives from DepartmentNamePageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public CreateModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        //public IActionResult OnGet()
+        //{
+        //ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+        //    return Page();
+        //}
+
+        public IActionResult OnGet()
+        {
+            /////////////////////////////////////////////////////////////
+            // Removes ViewData["DepartmentID"].
+            // The DepartmentNameSL SelectList is a strongly typed model
+            // and will be used by the Razor page.
+            // Strongly typed models are preferred over weakly typed. 
+            /////////////////////////////////////////////////////////////
+            PopulateDepartmentsDropDownList(_context);
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        //public async Task<IActionResult> OnPostAsync()
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return Page();
+        //    }
+
+        //    _context.Courses.Add(Course);
+        //    await _context.SaveChangesAsync();
+
+        //    return RedirectToPage("./Index");
+        //}
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var emptyCourse = new Course();
+
+            // Uses TryUpdateModelAsync to prevent overposting
+            if (await TryUpdateModelAsync<Course>(
+                 emptyCourse,
+                 "course",   // Prefix for form value.
+                 s => s.CourseID, s => s.DepartmentID, s => s.Title, s => s.Credits))
+            {
+                _context.Courses.Add(emptyCourse);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            /////////////////////////////////////////////////////////////
+            // Select DepartmentID if TryUpdateModelAsync fails.
+            // Removes ViewData["DepartmentID"].
+            // The DepartmentNameSL SelectList is a strongly typed model.
+            // and will be used by the Razor page.
+            // Strongly typed models are preferred over weakly typed. 
+            /////////////////////////////////////////////////////////////
+            PopulateDepartmentsDropDownList(_context, emptyCourse.DepartmentID);
+            return Page();
+        }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        ////////////////////////////////////////////////////
+        // OPTION #1:
+        ////////////////////////////////////////////////////
+        public IList<Course> Courses { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            this.Courses = await _context.Courses
+                            .Include(c => c.Department)
+                            .AsNoTracking()
+                            .ToListAsync();
+        }
+
+        ////////////////////////////////////////////////////
+        // OPTION #2:
+        ////////////////////////////////////////////////////
+        //public IList<CourseVM> Courses { get; set; }
+        //public async Task OnGetAsync()
+        //{
+        //    this.Courses = await _context.Courses
+        //    .Select(p => new CourseVM
+        //    {
+        //        CourseID = p.CourseID,
+        //        Title = p.Title,
+        //        Credits = p.Credits,
+        //        DepartmentName = p.Department.Name
+        //    }).ToListAsync();
+        //}
+    }
+
+}
