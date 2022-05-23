5a570b8ae9f83899f4b7f998a9e535c45aebc0ff
LeeTrent <lee.trent.1@gmail.com>
2022-05-23 07:32:25 -0500

    Created view models to correspond with EF entitites

---
+++ ContosoUniversity/ContosoUniversity/ViewModels/EnrollmentVM.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.ViewModels
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class EnrollmentVM
+    {
+        public int EnrollmentID { get; set; }
+
+
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+
+        /////////////////////////////////////////////////////////////////////////
+        // An enrollment record is for one course,
+        // so there's a CourseID FK property and a Course navigation property
+        /////////////////////////////////////////////////////////////////////////
+        public int CourseID { get; set; }
+        public CourseVM Course { get; set; }
+
+
+        /////////////////////////////////////////////////////////////////////////
+        // An enrollment record is for one student,
+        // so there's a StudentID FK property and a Student navigation property
+        /////////////////////////////////////////////////////////////////////////
+        public int StudentID { get; set; }
+        public StudentVM Student { get; set; }
+    }
+}
