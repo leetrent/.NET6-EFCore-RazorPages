f15a7a8f78431128a40f3220eb476eef58fd9acf
LeeTrent <lee.trent.1@gmail.com>
2022-06-07 08:41:22 -0500

    Moved appsettings.json to a location outside of the project so it's not save in GitHub

--- ContosoUniversity/ContosoUniversity/Pages/Index.cshtml.cs
+++ ContosoUniversity/ContosoUniversity/Pages/Index.cshtml.cs
@@ -1,20 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace ContosoUniversity.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
             _logger = logger;
+            _configuration = config;
         }
 
         public void OnGet()
         {
+            string logSnippet = "[Index][GET] => ";
 
+            string connectionString = _configuration.GetConnectionString("SchoolContext");
+
+            Console.WriteLine();
+            Console.WriteLine($"{logSnippet} (connectionString): '{connectionString}'");
+            Console.WriteLine();
+
+            string appsettingsDirectory = _configuration.GetValue<string>("APPSETTINGS_DIRECTORY");
+
+            Console.WriteLine();
+            Console.WriteLine($"{logSnippet} (appsettingsDirectory): '{appsettingsDirectory}'");
+            Console.WriteLine();
         }
     }
 }
